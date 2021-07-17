9c23cd89eff48fa9025a25e26acdb0a68b829f76
CloudMaX <yunma10@outlook.com>
2021-07-17 16:10:00 -0700

    [Update] Player Character Movement
    + Player Character Movement with Blend Space.
    + GunBase Class and Intialize BP_Rifle

---
+++ Source/SimpleShooter/GameModes/TPSDemoGameMode.h
@@ -0,0 +1,17 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/GameModeBase.h"
+#include "TPSDemoGameMode.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class SIMPLESHOOTER_API ATPSDemoGameMode : public AGameModeBase
+{
+	GENERATED_BODY()
+	
+};
